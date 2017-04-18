ecc2a6a2816e0eb6de86cbe85311dd1b51b04026
olaforest <olaforest@expedia.com>
2017-04-18 13:03:59 -0700

    Get pull requests working.

---
+++ src/main/java/com/expedia/eps/github/connectors/GithubApi.java
@@ -0,0 +1,16 @@
+package com.expedia.eps.github.connectors;
+
+import com.expedia.eps.github.metrics.models.PullRequest;
+
+import java.util.List;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+@Headers({"Authorization: Basic b2xhZm9yZXN0OjBhYzUzOTVjYWMxZThmMTM1YTIwMmUzMTc0NTRiNTYyZDAwZDViYjA=", "Content-Type: application/json"})
+public interface GithubApi {
+
+    @RequestLine("GET /repos/{owner}/{repo}/pulls?state={state}")
+    List<PullRequest> getPullRequests(@Param("owner") String owner, @Param("repo") String repo, @Param("state") String state);
+}
--- src/main/java/com/expedia/eps/github/metrics/Starter.java
+++ src/main/java/com/expedia/eps/github/metrics/Starter.java
@@ -1,8 +1,23 @@
 package com.expedia.eps.github.metrics;
 
+import com.expedia.eps.github.connectors.GithubApi;
+import com.expedia.eps.github.metrics.models.PullRequest;
+
+import java.util.List;
+
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+
 public class Starter {
 
     public static void main(String... args) {
 
+        final GithubApi githubApi = Feign.builder()
+                .decoder(new JacksonDecoder())
+                .target(GithubApi.class, "https://ewegithub.sb.karmalab.net/api/v3");
+
+        final List<PullRequest> pullRequests = githubApi.getPullRequests("EWE", "eps-booking-notification-service", "closed");
+
+        pullRequests.forEach(System.out::println);
     }
 }
